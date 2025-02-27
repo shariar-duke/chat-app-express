b98aa04d65fd5f61870f137c4021ecab0c634e70
Shariar Duke <shariarmahmud225@gmail.com>
2025-02-28 01:33:11 +0600

    inboxRouter

---
+++ router/inboxRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// internal imports 
+const {getInbox} = require("../controller/inboxController")
+
+const router = express.Router();
+
+// login page 
+router.get("/", getInbox )
+
+
+module.exports = router;
