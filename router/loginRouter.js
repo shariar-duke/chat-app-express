405b3219934de4fb52b43d942cddd672a14b6d2a
Shariar Duke <shariarmahmud225@gmail.com>
2025-02-28 01:06:03 +0600

    login router

---
+++ router/loginRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// internal imports 
+const {getLogin} = require("../controller/loginController")
+
+const router = express.Router();
+
+// login page 
+router.get("/", getLogin )
+
+
+module.exports = router;
