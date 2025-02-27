62a2b6aad87335209d18fc7ef890dded0a300f6f
Shariar Duke <shariarmahmud225@gmail.com>
2025-02-28 01:34:30 +0600

    app.js updated

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const loginRouter = require("./router/loginRouter")
+const usersRouter = require("./router/usersRouter")
+const inboxRouter = require("./router/inboxRouter")
 
 // internal exports 
 const {notFoundHandler, errorHandler } = require("./middlewares/common/errorHandler")
@@ -37,6 +39,8 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routing setup 
 app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
 
 //404 not found handler 
