296b4b9a7d22ca2350c04c537775b7bce8327b27
Shariar Duke <shariarmahmud225@gmail.com>
2025-02-28 10:09:40 +0600

    user validator

---
+++ middlewares/users/userValidator.js
@@ -0,0 +1,54 @@
+// external imports
+const { check, } = require("express-validator");
+
+
+
+// add user
+const addUserValidators = [
+  check("name")
+    .isLength({ min: 1 })
+    .withMessage("Name is required")
+    .isAlpha("en-US", { ignore: " -" })
+    .withMessage("Name must not contain anything other than alphabet")
+    .trim(),
+  check("email")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .trim()
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          throw createError("Email already is use!");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
+  check("mobile")
+    .isMobilePhone("bn-BD", {
+      strictMode: true,
+    })
+    .withMessage("Mobile number must be a valid Bangladeshi mobile number")
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ mobile: value });
+        if (user) {
+          throw createError("Mobile already is use!");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
+  check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+    ),
+];
+
+
+
+module.exports = {
+  addUserValidators,
+};
