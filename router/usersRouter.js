2cd8bc28b842ed7be9e66387911b80f2466a3a08
Shariar Duke <shariarmahmud225@gmail.com>
2025-02-28 10:13:45 +0600

    add user validation handler

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
+
 // internal imports 
-const {getUsers} = require("../controller/usersController")
+const {getUsers} = require("../controller/usersController");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const { addUserValidators, addUserValidationHandler ,  } = require("../middlewares/users/userValidator");
 
 const router = express.Router();
 
 // login page 
 router.get("/", getUsers )
+
+// add user 
+router.post("/", avatarUpload , addUserValidators , addUserValidationHandler)
 
 
 module.exports = router;
